68c1c88ecb276f37f399f4da8238be0d3e007f1b
Albie White <20148957@tafe.wa.edu.au>
2025-06-18 10:07:21 +0800

    feat/added constructors for sensor and display classes

--- src/display.py
+++ src/display.py
@@ -1,2 +1,8 @@
 class Display:
-    pass
+    def __init__(self, id_num, message=" ", is_on=False):
+        self.id_num = id_num
+        self.message = message
+        self.is_on = is_on
+
+    def __str__(self):
+        return f"Display {self.id_num}: {self.message}"
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,14 @@
 class Sensor:
+    def __init__(self, id_num, is_active, car_park):
+        self.id_num = id_num
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        return f"Sensor {self.id_num} is active: {self.is_active}"
+
+class EntrySensor(Sensor):
+    pass
+
+class ExitSensor(Sensor):
     pass
