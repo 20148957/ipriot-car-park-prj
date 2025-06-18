601218dd8b63cf1663c74570fbb6da438d29de3c
Albie White <20148957@tafe.wa.edu.au>
2025-06-18 09:44:30 +0800

    feature/added init and string method to CarPark class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,9 @@
 class CarPark:
-    pass
+    def __init__(self, location, capacity, plates=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or None
+        self.displays = displays or None
+
+    def __str__(self):
+        return f"{self.location} {self.capacity}"
