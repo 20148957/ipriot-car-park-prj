6f9b2e7a8b9cc4f11ccbe43eade5db588b4164f9
Albie White <20148957@tafe.wa.edu.au>
2025-06-19 07:21:56 +0800

    feat/added methods to the car_park class

--- src/car_park.py
+++ src/car_park.py
@@ -8,13 +8,32 @@ class CarPark:
         self.plates = plates or None
         self.displays = displays or None
 
-def register(self, component):
-    if not isinstance(component, (Sensor, Display)):
-        raise TypeError("Object must be a Sensor or Display")
-    if isinstance(component, Sensor):
-        self.sensors.append(component)
-    elif isinstance(component, Display):
-        self.displays.append(component)
+    def register(self, component):
+        if not isinstance(component, (Sensor, Display)):
+            raise TypeError("Object must be a Sensor or Display")
+        if isinstance(component, Sensor):
+            self.sensors.append(component)
+        elif isinstance(component, Display):
+            self.displays.append(component)
 
-def __str__(self):
-    return f"{self.location} {self.capacity}"
+    def add_car(self, plate):
+       self.plates.append(plate)
+       self.update_displays()
+
+    def remove_car(self, plate):
+        self.plates.remove(plate)
+        self.update_displays()
+
+    @property
+    def available_bays(self):
+        if self.capacity < len(self.plates):
+            return self.capacity - len(self.plates)
+        else:
+            return 0
+
+    def update_displays(self):
+        data = {"available_bays": self.available_bays, "temperature": 25}
+        for display in self.displays: display.update(data)
+
+    def __str__(self):
+        return f"{self.location} {self.capacity}"
