1fa770550e152aab6b8c768686341dc28e540550
Albie White <20148957@tafe.wa.edu.au>
2025-06-20 06:55:11 +0800

    Added logging functionality

--- src/car_park.py
+++ src/car_park.py
@@ -1,14 +1,17 @@
 from sensor import Sensor
 from display import Display
+from pathlib import Path
+from datetime import datetime # we'll use this to timestamp entries
 
 class CarPark:
-    def __init__(self, location, capacity, plates=None, displays=None, sensors=None, log_file=None):
+    def __init__(self, location, capacity, plates=None, displays=None, sensors=None, log_file=Path("log.txt")):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
         self.displays = displays or []
         self.sensors = sensors or []
-        self.log_file = log_file or "new_log.txt"
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        self.log_file.touch(exist_ok=True)
 
     def register(self, component):
         if not isinstance(component, (Sensor, Display)):
@@ -21,10 +24,12 @@ class CarPark:
     def add_car(self, plate):
        self.plates.append(plate)
        self.update_displays()
+       self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         self.plates.remove(plate)
         self.update_displays()
+        self._log_car_activity(plate, "exited")
 
     @property
     def available_bays(self):
@@ -36,6 +41,10 @@ class CarPark:
     def update_displays(self):
         data = {"available_bays": self.available_bays, "temperature": 25}
         for display in self.displays: display.update(data)
+
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
 
     def __str__(self):
         return f"{self.location} {self.capacity}"
