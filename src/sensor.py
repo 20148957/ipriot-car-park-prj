fd254acb100dd0d2ddf96b4b26c3d1352fb6bcc1
Albie White <20148957@tafe.wa.edu.au>
2025-06-19 13:59:48 +0800

    feat/added methods to sensor class

--- src/sensor.py
+++ src/sensor.py
@@ -1,14 +1,35 @@
+from abc import ABC, abstractmethod
+import random
+
 class Sensor:
     def __init__(self, id_num, is_active, car_park):
         self.id_num = id_num
         self.is_active = is_active
         self.car_park = car_park
 
+    @abstractmethod
+    def update_car_park(self, plate):
+        pass
+
+    def _scan_plate(self):
+        return 'FAKE-' + format(random.randint(0, 999), "03d")
+
+    def detect_vehicle(self):
+        plate = self._scan_plate()
+        self.update_car_park(plate)
+
     def __str__(self):
         return f"Sensor {self.id_num} is active: {self.is_active}"
 
 class EntrySensor(Sensor):
-    pass
+    def update_car_park(self, plate):
+        self.car_park.add_car(plate)
+        print(f"Incoming 🚘 vehicle detected. Plate: {plate}")
 
 class ExitSensor(Sensor):
-    pass
+    def update_car_park(self, plate):
+        self.car_park.remove_car(plate)
+        print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
+
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
