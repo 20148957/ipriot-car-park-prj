293a7760fe14f7b32656c03c3467db144ca36ae2
Albie White <20148957@tafe.wa.edu.au>
2025-06-20 10:07:05 +0800

    Added methods to the car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,7 +1,8 @@
 from sensor import Sensor
 from display import Display
 from pathlib import Path
-from datetime import datetime # we'll use this to timestamp entries
+from datetime import datetime
+import json
 
 class CarPark:
     def __init__(self, location, capacity, plates=None, displays=None, sensors=None, log_file=Path("log.txt")):
@@ -45,6 +46,19 @@ class CarPark:
     def _log_car_activity(self, plate, action):
         with self.log_file.open("a") as f:
             f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
+
+    def write_config(self):
+        with open("config.json", "w") as f:  # TODO: use self.config_file; use Path; add optional parm to __init__
+            json.dump({"location": self.location,
+                       "capacity": self.capacity,
+                       "log_file": str(self.log_file)}, f)
+
+    @classmethod
+    def from_config(cls, config_file=Path("config.json")):
+        config_file = config_file if isinstance(config_file, Path) else Path(config_file)
+        with config_file.open() as f:
+            config = json.load(f)
+        return cls(config["location"], config["capacity"], log_file=config["log_file"])
 
     def __str__(self):
         return f"{self.location} {self.capacity}"
